ca14eb555201fccc6b5fdda9dd90c9583462a952
SaniyaVishwakarma07 <saniyavishwakarma457@gmail.com>
2024-10-12 23:16:09 +0530

    Add files via upload

---
+++ ComputerMind.py
@@ -0,0 +1,19 @@
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Yay! The computer guessed your number, {guess}, correctly! ')
+
+x = int(input('Enter a last number until which you want to guess ? '))
+computer_guess(x)
